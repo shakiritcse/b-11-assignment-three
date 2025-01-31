90051256d39c1ba79e7314a898b5885ad88fca22
Shakir Al Hasan <shakiritcse@gmail.com>
2025-02-01 00:42:26 +0600

    footer done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'custom-gradient': 'linear-gradient(134.71deg, #F8CCFB 1.286%, #ACD2FD 100%)',
+        },
+      
+      },
+    },
+    plugins: [],
+  }
+  
